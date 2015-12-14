6864d62a12123167bfe1e13ed9ab6205181fb337
zusp <zusongpeng@gmail.com>
2015-12-15 00:25:06 +0800

    Add the initial gift_lib python.

---
+++ GIFT_PYTHON/GIFT_lib.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+GIFT_lib
+
+GIFT_lib is a python package to run the GIFT algorithm.
+https://github.com/songpeng/GIFT
+"""
+
+import pubchemfp # TODO: get compounds' pubchem fingerprints
+import rdkitfp # TODO: get compounds' rdkit based fingerprints
+import chemfp.bitops as chembit # fast calculate the jaccard disctance
+import argparse
+import numpy
+
+__author__ = "Songpeng Zu"
+__copyright__ = "Copyright 2015, Bioinformatics Lab, Department of Automation, Tsinghua University"
+__version__ = "1.1"
+__maintainer__ = "Songpeng Zu"
+__email__ = "zusongpeng@gmail.com"
+__status__ = "Development"
+
+class gift_em:
+    """Class to represent the elements needed in the algorithm.
+    """
+    def __init__(self,snum,dnum):
+        pass
---
+++ GIFT_PYTHON/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""GIFT_lib.py: PYTHON Package for GITT algorithm.
+
+GIFT is a statistical model to unconver the interactions between drug substructres
+and protein domains underlying the known drug-protein interactions.
+https://github.com/songpeng/GIFT
+"""
+
+__author__ = "Songpeng Zu"
+__copyright__ = "Copyright 2015, Bioinformatics Lab, Department of Automation, Tsinghua University"
+__version__ = "1.1"
+__maintainer__ = "Songpeng Zu"
+__email__ = "zusongpeng@gmail.com"
+__status__ = "Development"
