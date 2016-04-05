a7e02b3d7c14aade0d52a347f9fcd0888ff78c2a
zusongpeng <zusongpeng@gmail.com>
2016-04-05 21:50:35 +0800

    Add main.cpp file.

---
+++ dev_version/dev_src/src/main.cpp
@@ -0,0 +1,9 @@
+// The main file for the method of gift.
+
+// Details can be found from the gift.h
+// Author: Songpeng Zu
+// Email: zusongpeng@gmail.com
+// Date: 2016-04-05
+
+#include<boost/program_options.hpp> // read the parameters
+#include "gift.h"
