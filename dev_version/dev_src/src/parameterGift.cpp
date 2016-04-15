687a05f4791ed513d65255ccdb704118cd598b54
zusongpeng <zusongpeng@gmail.com>
2016-04-16 00:38:40 +0800

    Compile successfully for gift.

--- dev_version/dev_src/src/parameterGift.cpp
+++ dev_version/dev_src/src/parameterGift.cpp
@@ -11,6 +11,31 @@ namespace gift{
     return b ? "true" : "false";
   } // end of function BoolToString
 
+  IntArrayList drug2proteinList;
+  IntArrayList drug2subList;
+  IntArrayList sub2drugList;
+  IntArrayList protein2domainList;
+  IntArrayList domain2proteinList;
+  numericMatrix drugSub2proteinSubMatrix;
+  numericMatrix observedDrug2ProteinMatrix;
+  numericMatrix vardrugSub2proteinSubMatrix;
+  std::vector<double> loglikelyArray;
+
+  name2IndexHash drugName2Index;
+  name2IndexHash proteinName2Index;
+  nameList drugNameList;
+  nameList proteinNameList;
+  nameList drugSubNameList;
+  nameList proteinSubNameList;
+
+  nameList predictDrugNameList;
+  nameList predictProteinNameList;
+
+  nameList predictDrugNameList_WithSubs;
+  nameList predictProteinNameList_WithSubs;
+  IntArrayList predictDrug2SubList;
+  IntArrayList predictProtein2SubList;
+
   parameters::parameters(const std::string configFile) throw(std::string) {
     std::cout<<"Now set parameters with configFile."<<std::endl;
     // use boost program_options to read configs from a given file.
