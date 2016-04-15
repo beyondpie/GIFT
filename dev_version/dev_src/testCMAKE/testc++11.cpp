a8f364876a246c8ec3fc4fbe2816fc9501347e54
zusongpeng <zusongpeng@gmail.com>
2016-04-15 14:43:22 +0800

    Complete the c++11 test on Mac through cmake.

--- dev_version/dev_src/testCMAKE/testc++11.cpp
+++ dev_version/dev_src/testCMAKE/testc++11.cpp
@@ -3,7 +3,7 @@
 
 int main(){
   std::cout<<"hi,songpeng, test c++11 under clang with cmake."<<std::endl;
-  std::vector<int> array(1,10);
+  std::vector<int> array(10,0);
   for(const auto & v : array){
     std::cout<<"output array element: "<<v<<std::endl;
   }
