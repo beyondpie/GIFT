b98f15fba9578f5c8b21e6c6d0cb4577d22f20d4
zusongpeng <zusongpeng@gmail.com>
2016-11-23 14:18:14 +0800

    mv GIFTinloop.m

--- app/GIFTinloop.m
+++
@@ -1,6 +0,0 @@
-function result = GIFTinloop(comi,proj)
-    global Drug2Sub Protein_Domain Sub2Domain_Recover
-    subs = Drug2Sub(comi,:);
-    doms = Protein_Domain(proj,:);
-    result = 1 - exp(sum(sum(log(1 - Sub2Domain_Recover(subs==1,doms==1)))));
-end
